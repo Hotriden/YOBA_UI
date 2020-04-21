0f11b6369d0ea2697846208811c239fa8fd6371b
Hotriden <kvpdotnet@gmail.com>
2020-04-22 01:55:53 +0300

    builded

---
+++ build/precache-manifest.e8469b683475842b27d9d0817b059773.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aec547c25a5817243d86ade74d7e9932",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b41b7075c3a72d6f0d28",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d81850b84dd74fc71223",
+    "url": "/static/js/2.eba8241f.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/static/js/2.eba8241f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b41b7075c3a72d6f0d28",
+    "url": "/static/js/main.0a7549f5.chunk.js"
+  },
+  {
+    "revision": "0aa5a835350295ce318e",
+    "url": "/static/js/runtime-main.ccad61ae.js"
+  }
+]);
