1791a0b6bd2ea1f5d9ddcb8c9bff62ed84f6918a
Hotriden <bloodridetv@gmail.com>
2020-06-23 17:55:22 +0300

    changed warehouse form

--- src/Components/WareHouseComponent/WareHouses.js
+++ src/Components/WareHouseComponent/WareHouses.js
@@ -11,16 +11,16 @@ import Grid from '@material-ui/core/Grid';
 import '../ComponentStyle.scss';
 
 // Generate Order Data
-function createData(id, date, from, shipTo, article, amount, value) {
-  return { id, date, from, shipTo, article, amount, value };
+function createData(id, number, date, from, shipTo, article, amount, value) {
+  return { id, number, date, from, shipTo, article, amount, value };
 }
 
 const rows = [
-  createData(0, '16 Mar, 2019', 'WareHouse-3', 'WareHouse-1', '3444899', '1000', 3000.20),
-  createData(1, '16 Mar, 2019', 'Supplier-10', 'WareHouse-3', '11188', '255', 30.20),
-  createData(2, '16 Mar, 2019', 'WareHouse-1', 'Customer-3', '9995123', '3', 90.32),
-  createData(3, '16 Mar, 2019', 'Supplier-1', 'WareHouse-6', '4889952', '1110', 990.10),
-  createData(4, '15 Mar, 2019', 'WareHouse-4', 'WareHouse-11', '33000', '330', 223.03),
+  createData(0, '000021', '16 Mar, 2019', 'WareHouse-3', 'WareHouse-1', '3444899', '1000', 3000.20),
+  createData(1, '000020', '16 Mar, 2019', 'Supplier-10', 'WareHouse-3', '11188', '255', 30.20),
+  createData(2, '000019', '16 Mar, 2019', 'WareHouse-1', 'Customer-3', '9995123', '3', 90.32),
+  createData(3, '000018', '16 Mar, 2019', 'Supplier-1', 'WareHouse-6', '4889952', '1110', 990.10),
+  createData(4, '000017', '15 Mar, 2019', 'WareHouse-4', 'WareHouse-11', '33000', '330', 223.03),
 ];
 
 export default function WareHouse() {
@@ -51,6 +51,7 @@ export default function WareHouse() {
           <Table size="small">
             <TableHead>
               <TableRow>
+                <TableCell>№</TableCell>
                 <TableCell>Date</TableCell>
                 <TableCell>From</TableCell>
                 <TableCell>To</TableCell>
@@ -62,6 +63,7 @@ export default function WareHouse() {
             <TableBody>
               {rows.map((row) => (
                 <TableRow key={row.id}>
+                  <TableCell>{row.number}</TableCell>
                   <TableCell>{row.date}</TableCell>
                   <TableCell>{row.from}</TableCell>
                   <TableCell>{row.shipTo}</TableCell>
@@ -80,6 +82,7 @@ export default function WareHouse() {
         <Table size="small">
           <TableHead>
             <TableRow>
+              <TableCell>№</TableCell>
               <TableCell>Date</TableCell>
               <TableCell>From</TableCell>
               <TableCell>To</TableCell>
@@ -91,6 +94,7 @@ export default function WareHouse() {
           <TableBody>
             {rows.map((row) => (
               <TableRow key={row.id}>
+                <TableCell>{row.number}</TableCell>
                 <TableCell>{row.date}</TableCell>
                 <TableCell>{row.from}</TableCell>
                 <TableCell>{row.shipTo}</TableCell>
