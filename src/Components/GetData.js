dd8b0e81bf02109dab7992f80932d987e3a6e44a
Hotriden <kvpdotnet@gmail.com>
2020-07-03 01:52:23 +0300

    changed for https

--- src/Components/GetData.js
+++ src/Components/GetData.js
@@ -4,14 +4,14 @@ import Cookies from 'universal-cookie';
 const cookies = new Cookies();
 
 export async function GetWareHouse() {
-    var result =  await axios.get('http://apiyoba.pp.ua/api/WareHouse/GetAll', { headers: { 'Authorization': 'Bearer ' + cookies.get('_uc')}}
+    var result =  await axios.get('https://apiyoba.pp.ua/api/WareHouse/GetAll', { headers: { 'Authorization': 'Bearer ' + cookies.get('_uc')}}
     );
     console.log(result.data);
     return result.data;
 }
 
 export async function GetSuppliers() {
-    var result =  await axios.get('http://apiyoba.pp.ua/api/Supplier/GetAll', { headers: { 'Authorization': 'Bearer ' + cookies.get('_uc')}}
+    var result =  await axios.get('https://apiyoba.pp.ua/api/Supplier/GetAll', { headers: { 'Authorization': 'Bearer ' + cookies.get('_uc')}}
     );
     console.log(result.data);
     return result.data;
